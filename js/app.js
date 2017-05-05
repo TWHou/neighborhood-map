91b1bc03592f0c7d6d3c224c7e6cc143b55e6d7d
Christina Hsieh <hsiehkunlun@hotmail.com>
2017-05-04 23:00:24 -0400

    add infowindow

--- js/app.js
+++ js/app.js
@@ -14,38 +14,63 @@ let Museum = function (data) {
     this.placeId = data.place_id;
     this.name = data.name;
     this.address = data.formatted_address;
+    this.hours = data.opening_hours ? data.opening_hours.weekday_text : [];
     this.visible = ko.observable(true);
+    this.wikiText = "";
+    this.wikiUrl = "";
     this.marker = new google.maps.Marker({
         map: map,
         title: data.name,
         position: data.geometry.location,
-        /*icon: {
-            url: data.icon,
-            size: new google.maps.Size(35,35),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(15, 15),
-            scaledSize: new google.maps.Size(25, 25)
-        },*/
         animation: google.maps.Animation.DROP
     });
 }
 
 let ViewModel = function() {
     let self = this;
+    this.infowindow = new google.maps.InfoWindow();
 
     this.museums = ko.observableArray();
+    this.currentMuseum = ko.observable(this.museums()[0]);
+
     placeIds.forEach(function(placeId) {
         let service = new google.maps.places.PlacesService(map);
         service.getDetails({
             placeId: placeId
         }, function(place, status) {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
-                self.museums.push(new Museum(place));
+                let newMuseum = new Museum(place);
+                newMuseum.marker.addListener('click', function(){
+                    self.handleClick(newMuseum);
+                });
+                self.museums.push(newMuseum);
             } else {
                 console.log(status);
             }
         });
     });
+
+    this.handleClick = function(museum) {
+        self.currentMuseum(museum);
+        self.openInfo(museum.marker, self.infowindow);
+    }
+
+    this.openInfo = function(marker, infowindow) {
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            let innerHTML = '<div><strong>';
+            innerHTML += self.currentMuseum().name;
+            innerHTML += '</strong><br><address>';
+            innerHTML += self.currentMuseum().address;
+            innerHTML += '</address><br><button data-bind="click: $root.toggleModal">More Details</button></div>';
+            infowindow.setContent(innerHTML);
+            infowindow.open(map, marker);
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
+        }
+    }
+
     this.filter = ko.observable("");
     this.filteredMuseums = ko.computed(function() {
         let filter = self.filter().toLowerCase();
@@ -53,9 +78,10 @@ let ViewModel = function() {
             return self.museums();
         }
     });
-
+    this.toggleModal = function() {
+        console.log("will open modal....");
+    }
     this.showModal = ko.observable(false);
-    this.currentMuseum = ko.observable(this.museums()[0]);
 }
 
 let map;
